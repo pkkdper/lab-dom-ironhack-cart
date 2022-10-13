bafbdf0f3d963312354c17185476c839bbc81458
pkkdpr <oliwia.giercza@gmail.com>
2022-10-12 21:22:37 -0500

    done - but how?

--- js/index.js
+++ js/index.js
@@ -1,30 +1,41 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-const price = document. querySelector('.price span')
-const quantity = document.querySelector('.quantity input')
-const priceValue = Number(price.innerText);
-const quantityValue = +quantity.value; 
-let subtotal = priceValue * quantityValue;
-let subtotalValue = document.querySelector('.subtotal');
-subtotalValue.innerHTML = subtotal;
-return subtotal;
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const priceValue = Number(price.innerText);
+  const quantityValue = +quantity.value;
+  let subtotal = priceValue * quantityValue;
+  let subtotalValue = product.querySelector('.subtotal span');
+  subtotalValue.innerText = subtotal;
+  return subtotal;
 }
 
-function calculateAll() {
+function calculateAll(product) {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
+  let total = 0;
+  // product.querySelectorAll('.product').forEach(function (node) {});
+  const allProducts = document.querySelectorAll('.product');
+  allProducts.forEach((element) => {
+ let subTotal = updateSubtotal(element)
+  total += subTotal    // adding the total from each element
+  })
   // ITERATION 2
+  // const allProducts = document.getElementsByClassName('product');
+  //   const allSubtotals = document.querySelectorAll('.subtotal span');
+  // const sum = allSubtotals.reduce((a,b) => a + b, 0);
+  // total = document.querySelector('.total-value span');
+  // total.innerText = sum}
+  // return sum
+  // // return updateSubtotal(allProducts)
   //... your code goes here
-
   // ITERATION 3
   //... your code goes here
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
